5d8c69878ddbedf5d8ab05e1a70932bf9f364f8d
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-10 15:05:30 -0500

    Change search placeholder text to fit on mobile

--- wp-content/themes/avl/searchform.php
+++ wp-content/themes/avl/searchform.php
@@ -1,4 +1,4 @@
 <form id="site-search" class="search-form" role="search" method="get" action="<?= home_url( '/' ); ?>">
-	<input type="search" class="form-control form-control-lg search-field" name="s" value="<?= get_search_query(); ?>" placeholder="Search The City of Asheville" aria-label="Search The City of Asheville" aria-describedby="search-button">
+	<input type="search" class="form-control form-control-lg search-field" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Search" aria-describedby="search-button">
 	<button class="btn" type="submit" id="search-button"><i class="icomoon icomoon-search"></i></button>
 </form>
