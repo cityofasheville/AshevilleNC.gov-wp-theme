aae68ecb148148628697db1b6f5832ae6c5b0960
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-14 15:24:30 -0500

    Force HTTPS

--- wp-config.php
+++ wp-config.php
@@ -147,6 +147,28 @@ if ( ! defined( 'WP_DEBUG' ) ) {
     define('WP_DEBUG', true);
 }
 
+// From https://pantheon.io/docs/http-to-https/ - MM
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+  // Redirect to https://$primary_domain in the Live environment
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+    /** Your registered domain name */
+    $primary_domain = 'beta.ashevillenc.gov';
+  }
+  else {
+    // Redirect to HTTPS on every Pantheon environment.
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  }
+
+  if ($_SERVER['HTTP_HOST'] != $primary_domain
+      || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+      || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
+
 
 /* That's all, stop editing! Happy Pressing. */
 
