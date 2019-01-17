feb9d591ebaf280cd2e8dbaa0e07a6d62742e2b0
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-17 16:54:37 -0500

    Ditch duplicate search button ids

--- wp-content/themes/avl/searchform-header.php
+++ wp-content/themes/avl/searchform-header.php
@@ -1,4 +1,4 @@
-<form id="site-search" class="search-form ml-2 mr-3" role="search" method="get" action="<?= home_url( '/' ); ?>">
-	<input type="search" class="form-control search-field" name="s" value="<?= get_search_query(); ?>" placeholder="Search">
-	<button class="btn" type="submit" id="search-button"><span class="icomoon icomoon-search"></span></button>
+<form id="header-site-search" class="search-form ml-2 mr-3" role="search" method="get" action="<?= home_url( '/' ); ?>">
+	<input type="search" class="form-control search-field" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Search">
+	<button class="btn search-button" type="submit"><span class="icomoon icomoon-search"></span></button>
 </form>
