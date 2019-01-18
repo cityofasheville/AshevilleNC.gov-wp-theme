0016139d87fb9282be6932bb1b22566c90a252f2
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-18 09:32:30 -0500

    Clarify aria label

--- wp-content/themes/avl/searchform.php
+++ wp-content/themes/avl/searchform.php
@@ -1,4 +1,4 @@
 <form id="site-search" class="search-form" role="combobox" aria-haspopup="listbox" method="get" action="<?= home_url( '/' ); ?>">
-	<input type="search" class="form-control form-control-lg search-field"  role="searchbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Search">
+	<input type="search" class="form-control form-control-lg search-field"  role="searchbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Site search">
 	<button class="btn search-button" type="submit"><span class="icomoon icomoon-search"></span></button>
 </form>
