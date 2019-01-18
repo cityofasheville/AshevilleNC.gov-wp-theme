0accaa01a38ba6f1c393ef221ccdab3fd1b17840
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-18 10:21:41 -0500

    Fix a few color contrast issues, search still bad

--- wp-content/themes/avl/searchform.php
+++ wp-content/themes/avl/searchform.php
@@ -1,4 +1,5 @@
-<form id="site-search" class="search-form" role="combobox" aria-haspopup="listbox" method="get" action="<?= home_url( '/' ); ?>">
-	<input type="search" class="form-control form-control-lg search-field"  role="searchbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Site search">
-	<button class="btn search-button" type="submit"><span class="icomoon icomoon-search"></span></button>
+<form id="site-search" class="search-form" role="combobox" aria-expanded="false" aria-haspopup="listbox" method="get" action="<?= home_url( '/' ); ?>">
+	<label for="site-search"><span class="visually-hidden">Search</span></label>
+	<input id="site-search" type="search" class="form-control form-control-lg search-field"  role="textbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Site search">
+	<button class="btn search-button" type="submit"><span class="visually-hidden">Search</span><span class="icomoon icomoon-search"></span></button>
 </form>
