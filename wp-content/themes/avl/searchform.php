945433f79691b5156cb258e93f214c70a8694261
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-18 09:31:19 -0500

    Correct aria roles for search maybe

--- wp-content/themes/avl/searchform.php
+++ wp-content/themes/avl/searchform.php
@@ -1,4 +1,4 @@
-<form id="site-search" class="search-form" role="search" method="get" action="<?= home_url( '/' ); ?>">
-	<input type="search" class="form-control form-control-lg search-field" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Search">
+<form id="site-search" class="search-form" role="combobox" aria-haspopup="listbox" method="get" action="<?= home_url( '/' ); ?>">
+	<input type="search" class="form-control form-control-lg search-field"  role="searchbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Search">
 	<button class="btn search-button" type="submit"><span class="icomoon icomoon-search"></span></button>
 </form>
