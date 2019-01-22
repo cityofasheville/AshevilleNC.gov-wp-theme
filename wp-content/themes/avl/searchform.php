8ec33dfce9053a96bf11a8e49c9a8fcf79d13ccc
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-22 10:59:51 -0500

    Correct search aria

--- wp-content/themes/avl/searchform.php
+++ wp-content/themes/avl/searchform.php
@@ -1,5 +1,35 @@
-<form id="site-search" class="search-form" role="combobox" aria-expanded="false" aria-haspopup="listbox" method="get" action="<?= home_url( '/' ); ?>">
-	<input id="site-search-input" type="search" class="form-control form-control-lg search-field" role="textbox" name="s" value="<?= get_search_query(); ?>" placeholder="Search" aria-label="Site search">
-	<label for="site-search-input" class="visually-hidden">Search</label>
-	<button class="btn search-button" type="submit"><span class="visually-hidden">Search</span><span class="icomoon icomoon-search"></span></button>
+<form
+	id="site-search"
+	class="search-form"
+	role="search"
+	aria-expanded="false"
+	aria-haspopup="listbox"
+	method="get"
+	action="<?= home_url( '/' ); ?>"
+>
+	<input
+		id="site-search-input"
+		type="search"
+		class="form-control form-control-lg search-field"
+		role="combobox"
+		aria-autocomplete="list"
+		aria-haspopup="true"
+		aria-controls="#algolia-autocomplete-listbox-0"
+		name="s"
+		value="<?= get_search_query(); ?>"
+		placeholder="Search"
+		aria-label="Site search"
+	>
+	<label for="site-search-input" class="visually-hidden">
+		Search
+	</label>
+	<button
+		class="btn search-button"
+		type="submit"
+	>
+		<span class="visually-hidden">
+			Search
+		</span>
+		<span class="icomoon icomoon-search"></span>
+	</button>
 </form>
