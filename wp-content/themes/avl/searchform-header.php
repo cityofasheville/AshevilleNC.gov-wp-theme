74b3b6c675d28d8a18a63a4c4fb7d22205284ea2
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-25 17:34:39 -0500

    Fix header search buttong

--- wp-content/themes/avl/searchform-header.php
+++ wp-content/themes/avl/searchform-header.php
@@ -1,35 +1,40 @@
 <form
 	id="header-site-search"
 	class="search-form ml-2 mr-3"
-	role="search"
 	aria-haspopup="listbox"
 	method="get"
 	action="<?= home_url( '/' ); ?>"
 >
 	<div
-		role="combobox"
+		role="search"
 	>
-		<input
-			id="header-site-search-input"
-			type="text"
-			class="form-control search-field"
-			aria-autocomplete="list"
-			name="s"
-			value="<?= get_search_query(); ?>"
-			placeholder="Search"
-			aria-label="Site search"
-		>
 		<label for="header-site-search-input" class="visually-hidden">
 			Search
 		</label>
-		<button
-			class="btn search-button"
-			type="submit"
+
+		<div
+			role="combobox"
+			class="input-group"
 		>
-			<span class="visually-hidden">
-				Search
-			</span>
-			<span class="icomoon icomoon-search"></span>
-		</button>
+			<input
+				id="header-site-search-input"
+				type="text"
+				class="form-control search-field rounded"
+				aria-autocomplete="list"
+				name="s"
+				value="<?= get_search_query(); ?>"
+				placeholder="Search"
+				aria-label="Site search"
+			>
+			<button
+				class="btn search-button"
+				type="submit"
+			>
+				<span class="visually-hidden">
+					Search
+				</span>
+				<span class="icomoon icomoon-search"></span>
+			</button>
+		</div>
 	</div>
 </form>
