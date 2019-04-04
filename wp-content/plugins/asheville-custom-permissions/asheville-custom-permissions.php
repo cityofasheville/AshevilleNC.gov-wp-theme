1d62a16e72958a6874abda1afa75fd9ed781d82d
Patrick Conant <conantp@prcappss-MacBook-Pro.local>
2019-04-04 18:42:30 -0400

    Creating plugin directory

---
+++ wp-content/plugins/asheville-custom-permissions/asheville-custom-permissions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Asheville Custom Permissions
+Plugin URI: http://www.ashevillenc.gov
+Description: This plugin contains the logic for Asheville custom permissions.
+Version: 0.1
+Author: Patrick Conant
+Author URI: https://www.prcapps.com
+License: GPL2
+*/
