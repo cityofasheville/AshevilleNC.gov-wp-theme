f1a880738b4ca1180a44317ccf992630a22923cd
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-05-01 14:10:10 -0400

    Delay loading of twitter

--- wp-content/themes/avl/js/custom-home.js
+++ wp-content/themes/avl/js/custom-home.js
@@ -1,12 +1,14 @@
-twttr.events.bind(
-	'rendered',
-	function (event) {
-		var target = jQuery('#'+ event.target.id)
-		// Hide the rendered twitter box and style it to match other social media icons
-		target.attr('tabindex', '-1');
-		target.css('border', '');
-		target.addClass('rounded');
-		target.css('height', '');
-		target.css('min-height', '');
-	}
-);
+jQuery(document).ready(function() {
+  twttr.events.bind(
+  	'rendered',
+  	function (event) {
+  		var target = jQuery('#'+ event.target.id)
+  		// Hide the rendered twitter box and style it to match other social media icons
+  		target.attr('tabindex', '-1');
+  		target.css('border', '');
+  		target.addClass('rounded');
+  		target.css('height', '');
+  		target.css('min-height', '');
+  	}
+  );
+});
